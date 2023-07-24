5daa34fd80e8b29b554fb420619fe92411c5730b
juansesu3 <juanseu3@hotmail.com>
2023-07-24 21:53:37 +0200

    FEATURE(backend setup)

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -3,9 +3,10 @@ import DashboardBox from "@/components/DashboardBox";
 type Props = {};
 
 const Row1 = (props: Props) => {
+  
   return (
     <>
-      <DashboardBox gridArea="a">aaa</DashboardBox>
+      <DashboardBox gridArea="a"></DashboardBox>
       <DashboardBox gridArea="b"></DashboardBox>
       <DashboardBox gridArea="c"></DashboardBox>
     </>
