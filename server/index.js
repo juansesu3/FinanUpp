73307c5f9d7b1ec436cd3b3135a7fba969750875
juansesu3 <juanseu3@hotmail.com>
2023-07-25 12:17:02 +0200

    FIX(mongoose version )

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
 /* -CONFIGURATIONS- */
+mongoose.Promise = global.Promise;
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -17,5 +18,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(async ()=>{
+  app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`));
+}).catch((error)=> console.log(`${error} did not connect`));
 
-/*MONGOOSE SETUP */
+
